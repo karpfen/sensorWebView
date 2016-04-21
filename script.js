45e7eaa9122b17839182739bfd21137c08e29a1f
Andreas Petutschnig <andreas@petutschnig.de>
2016-04-21 17:37:44 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+ * Project: Urban Emotions Web Viewer
+ * 
+ * The Urban Emotions Web Viewer is free software. It can be redistributed and
+ * modified under the terms of the GNU General Public Licence as published by the
+ * Free Software Foundation, either Version 3 of the license, or (at your option)
+ * any later version.
+ * 
+ * For the license, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author: Andreas Petutschnig
+ * Email: andreas@petutschnig.de
+ * 
+ * Description: This web viewer can be used to call a web processing service to
+ * generate images derived from sensor data and display this data in a web map.
+ */
+
