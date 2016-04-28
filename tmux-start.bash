dd435faabcd8a090338fc4e12bde330a2171f073
Andreas Petutschnig <andreas@petutschnig.de>
2016-04-28 11:17:29 +0200

    update tmux-start.bash

--- tmux-start.bash
+++ tmux-start.bash
@@ -7,6 +7,7 @@ tmux new-window -t $SESSION:1 -k -n main
 tmux send-keys -t $SESSION:1 'vim index.html' C-m
 tmux split-window -h
 tmux select-pane -t 1
-tmux send-keys -t $SESSION: 'vim script.js' C-m
+tmux send-keys -t $SESSION: 'vim script/init_osm_map.js' C-m
 tmux split-window -v
+firefox index.html &
 tmux attach -t $SESSION
