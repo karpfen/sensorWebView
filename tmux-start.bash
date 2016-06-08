2ba79f7df0075f78cd47e82d0c77a81d37038ba7
Andreas Petutschnig <andreas@petutschnig.de>
2016-06-08 15:25:01 +0200

    use standard browser

--- tmux-start.bash
+++ tmux-start.bash
@@ -9,5 +9,5 @@ tmux split-window -h
 tmux select-pane -t 1
 tmux send-keys -t $SESSION: 'vim js/init_osm_map.js' C-m
 tmux split-window -v
-firefox http://localhost:8080/sensorWebView/ &
+xdg-open http://localhost:8080/sensorWebView/ &
 tmux attach -t $SESSION
